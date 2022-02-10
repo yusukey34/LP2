1de34e085c006dcb3043ee06aeedddb775c525fd
yusuke.tochigi <tochigi0394@gmail.com>
2022-02-10 19:03:18 +0900

    first-commit

---
+++ js/common.js
@@ -0,0 +1,21 @@
+  
+window.onunload = function() {};
+function sizecheck() {
+  w = window.innerWidth ? window.innerWidth : $(window).width();
+  h = window.innerHeight ? window.innerHeight : $(window).height();
+}
+
+sizecheck();
+$(function () {
+  if (w > 769) {
+} else { 
+  $("header .drawer").click(function(){
+  $('body').toggleClass('nav-open');
+  $('header .header_nav').fadeToggle(200);
+    });
+  $("header li a").click(function(){
+  $('body').toggleClass('nav-open');
+  $('header .header_nav').fadeToggle(200);
+    });
+  }
+}); 
